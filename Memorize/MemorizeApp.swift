60a8619fad5fac963d5d45b36886718eb3bc8be4
oluwatayo <oluwatayoadegboye@gmail.com>
2023-06-12 23:30:13 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Oluwatayo Adegboye on 12/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
